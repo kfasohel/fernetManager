6465f185fcad4bc06ca241f20dd829158d855d14
K F A SOHEL <kfasohel@Sohels-MacBook-Air.local>
2024-03-02 10:44:22 +0600

    Added some getters

--- password_class.py
+++ password_class.py
@@ -1,3 +1,4 @@
+import sqlite3
 import base64
 import os
 import sys
@@ -7,12 +8,27 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+
 class PasswordClass:
     def __init__(self) -> None:
-        self.key = None
-        self.salt = os.urandom(16)
+        self._key = None
+        self._salt = os.urandom(16)
         # Configure CS50 Library to use SQLite database
-        self.db = SQL("sqlite:///passwords.db") or None
+        # self.db = SQL("sqlite:///passwords.db") or None
+        conn = sqlite3.connect('users.db')
+        self._db = conn.cursor()
+
+    @property
+    def key(self):
+        return self._key
+
+    @property
+    def salt(self):
+        return self._salt
+
+    @property
+    def db(self):
+        return (f"It is a {type(self._db)} file")
 
     # Create Fernet key
     def create_key(self, u_password):
